398342f6821da04ea32da81eb35793e65d3d0ac6
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2022-02-01 19:24:26 -0300

    more packages added to the script and hostname and user are now variables

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+hostname=
+user=
+
+
+
 #time zone
 ln -sf usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
@@ -11,4 +16,37 @@ locale-gen
 echo LANG=pt_BR.UTF-8 > /etc/locale.conf
 
 #hostname and hosts file
-echo arch > /etc/hostname
+echo $hostname > /etc/hostname
+echo "127.0.0.1	localhost.localdomain	localhost" >> /etc/hosts
+echo "::1		localhost.localdomain	localhost" >> /etc/hosts
+echo "127.0.1.1	$hostname.localdomain	$hostname" >> /etc/hosts
+
+#packages
+#shell
+pacman -S zsh --noconfirm
+#nvidia
+pacman -S nvidia nvidia-utils --noconfirm
+#xdg
+pacman -S xdg-utils xdg-user-dirs --noconfirm
+#browser
+pacman -S firefox --noconfirm
+#i3 gaps and xorg
+pacman -S xorg xorg-init xterm i3-gaps i3-status ttf-dejavu --noconfirm
+#lightdm and greeter
+
+
+#sound - pipewire and alsa and pulseaudio volume control
+pacman -S alsa-utils pipewire wireplumber pipewire-alsa pipewire-pulse --noconfirm
+#terminal
+pacman -S alacritty --noconfirm
+#rofi and dmenu - dmenu if rofi stops working, just in case
+pacman -S rofi dmenu --noconfirm
+
+
+
+
+
+
+#install network manager and enable it
+pacman -S networkmanager network-manager-applet --noconfirm
+systemctl enable NetworkManager
