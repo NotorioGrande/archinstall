86303ac9b420cb55f5515382949656f11a16243d
Gustavo Grande <gustavobgrande@hotmail.com>
2022-02-02 16:22:24 -0300

    display manager

--- install.sh
+++ install.sh
@@ -8,19 +8,19 @@ user=
 #time zone
 ln -sf usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
-
+echo "Time zone updated..."
 #localization 
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen #uncomment en_US UTF-8 line
 sed -i 's/#pt_BR.UTF-8 UTF-8/pt_PT.UTF-8 UTF-8/g' /etc/locale.gen #uncomment pt_BR UTF-8 line
 locale-gen
 echo LANG=pt_BR.UTF-8 > /etc/locale.conf
-
+echo "Localization configured"
 #hostname and hosts file
 echo $hostname > /etc/hostname
 echo "127.0.0.1	localhost.localdomain	localhost" >> /etc/hosts
 echo "::1		localhost.localdomain	localhost" >> /etc/hosts
 echo "127.0.1.1	$hostname.localdomain	$hostname" >> /etc/hosts
-
+echo "Hostname and hosts file created"
 #packages
 #shell
 pacman -S zsh --noconfirm
@@ -33,8 +33,10 @@ pacman -S firefox --noconfirm
 #i3 gaps and xorg
 pacman -S xorg xorg-init xterm i3-gaps i3-status ttf-dejavu --noconfirm
 #lightdm and greeter
+pacman -S lightdm lightdm-webkit2-greeter --noconfirm
+systemctl enable lightdm.service
 
-
+sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/' /etc/lightdm/lightdm.conf
 #sound - pipewire and alsa and pulseaudio volume control
 pacman -S alsa-utils pipewire wireplumber pipewire-alsa pipewire-pulse --noconfirm
 #terminal
